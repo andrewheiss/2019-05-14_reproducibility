f73a6f61785ed42d316cb2276f7a3b17928d0d58
Andrew Heiss <andrew@andrewheiss.com>
2019-05-14 17:24:51 -0600

    Add rrtools example

---
+++ 06_rrtools-package/rrtoolsmagic/R/graphics.R
@@ -0,0 +1,35 @@
+#' theme_weather
+#'
+#' A custom ggplot2 theme used throughout this project
+#'
+#' @param base_size base font size (default is 9)
+#'
+#' @export
+#'
+#' @examples
+#' library(ggplot2)
+#'
+#' ggplot(mtcars, aes(x = mpg, y = wt)) +
+#'   geom_point() +
+#'   theme_weather()
+theme_weather <- function(base_size = 9) {
+  ret <- theme_bw(base_size) +
+    theme(plot.title = element_text(size = rel(1.4), face = "bold"),
+          plot.subtitle = element_text(size = rel(1)),
+          plot.caption = element_text(size = rel(0.8), color = "grey50",
+                                      face = "plain", margin = margin(t = 10)),
+          panel.border = element_blank(),
+          panel.spacing = unit(1, "lines"),
+          panel.grid.minor = element_blank(),
+          strip.text = element_text(size = rel(0.9), hjust = 0, face = "bold"),
+          strip.background = element_rect(fill = "#ffffff", colour = NA),
+          axis.ticks = element_blank(),
+          axis.title.x = element_text(margin = margin(t = 10)),
+          legend.key = element_blank(),
+          legend.text = element_text(size = rel(1)),
+          legend.spacing = unit(0.1, "lines"),
+          legend.box.margin = margin(t = -0.5, unit = "lines"),
+          legend.margin = margin(t = 0))
+
+  ret
+}
